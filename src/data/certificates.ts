4f2124ec30d843f574bf1aef2cf7ca2f9f3eb9ab
Boris Shagiev <boris260692@gmail.com>
2023-07-19 20:55:32 +0300

    Add new certificates

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -8,4 +8,14 @@ export const certificates = [
         title: 'HTML5/CSS3',
         path: 'https://testprovider.com/ru/search-certificate/tp44842082'
     },
+
+    {
+        title: 'CIKLUM & Prometheus',
+        path: 'https://drive.google.com/file/d/1CF1BvP8gdErhB4R_tySWW1gK3MlUfXCE/view'
+    },
+
+    {
+        title: 'CURSOR EDUCATION',
+        path: 'https://drive.google.com/file/d/1wwoP-qJi7Y947XfIJAf9pQEcGrg92ghu/view'
+    },
 ]
